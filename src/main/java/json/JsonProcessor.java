73172ecd774025ee2c235b33f4029d6049a3defe
Asif Imtiaz Shaafi <shaafi.20994@gmail.com>
2024-04-18 00:26:27 -0500

    Update the JsonProcessor to parse real trace correctly

--- src/main/java/json/JsonProcessor.java
+++ src/main/java/json/JsonProcessor.java
@@ -56,7 +56,15 @@ public class JsonProcessor {
         for (int i = 0; i < spans.length(); i++) {
             JSONObject span = spans.getJSONObject(i);
             String spanID = span.getString("spanID");
-            String returnCode = span.getJSONArray("tags").getJSONObject(0).getString("value");
+            JSONArray tags = span.getJSONArray("tags");
+
+            String returnCode = "200";
+
+            for (int j = 0; j < tags.length(); j++) {
+                if (tags.getJSONObject(j).has("http.status_code")) {
+                    returnCode = tags.getJSONObject(j).getString("http.status_code");
+                }
+            }
 
             if (returnCode.startsWith("5")) {
                 continue;
@@ -79,6 +87,8 @@ public class JsonProcessor {
             JSONArray references = span.getJSONArray("references");
             for (int j = 0; j < references.length(); j++) {
                 JSONObject reference = references.getJSONObject(j);
+//                System.out.println(reference);
+//                System.out.println(reference.getString("spanID"));
                 childrenMap.get(reference.getString("spanID")).add(spanID);
             }
 
