a1445346b1d66222712e9bd3dbb538aa66617798
Asif Imtiaz Shaafi <shaafi.20994@gmail.com>
2024-05-05 23:12:25 -0500

    Add new trace data

--- src/main/java/json/RunJsonProcessor.java
+++ src/main/java/json/RunJsonProcessor.java
@@ -4,8 +4,8 @@ import static json.RatioCalculator.FILE_PATH;
 
 public class RunJsonProcessor {
     public static void main(String[] args) {
-        String inputFile = FILE_PATH + "Test2.json";
-        String outputFile = FILE_PATH + "alltraces_parsed.json";
+        String inputFile = FILE_PATH + "order_trace_diff_path.json";
+        String outputFile = FILE_PATH + "order_trace_diff_compare.json";
 
         JsonProcessor processor = new JsonProcessor(inputFile, outputFile);
         try {
